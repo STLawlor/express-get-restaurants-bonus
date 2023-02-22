751f91bebe530b7934c7051abf38a28ddbac9e92
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-02-22 14:13:22 +0000

    add endpoint for loaded restaurant data

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const port = 3000;
-const {Restaurant, Menu, Item} = require("./models/index")
-const {sequelize} = require("./db")
+const { Restaurant, Menu, Item } = require("./models/index");
+const { sequelize } = require("./db");
 
-//TODO: 
+//TODO:
+app.get("/restaurants", async (req, res) => {
+  try {
+    let restaurants = await Restaurant.findAll({
+      include: [{ model: Menu, include: [{ model: Item }] }],
+    });
+    res.json(restaurants);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
-    sequelize.sync()
-    console.log("App listening on port " + port)
-})
+  sequelize.sync();
+  console.log("App listening on port " + port);
+});
